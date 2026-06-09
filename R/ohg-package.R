f5afafdd850c3374c25a842a8adcc9c6d9a67942
Mahmoud Eladawi <mel3dwi@gmail.com>
2026-06-09 04:10:50 +0300

    Package set up

---
+++ R/ohg-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom methods is
+#' @importFrom stats phyper p.adjust p.adjust.methods median mad
+## usethis namespace: end
+NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ohg)
+
+test_check("ohg")
